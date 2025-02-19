92c3c1becb712f6125cacc7660c66f9ce082f41d
meya <meya@kth.se>
2025-02-19 12:14:12 +0100

    Added an about button

--- maestri-app/src/components/Navbar.tsx
+++ maestri-app/src/components/Navbar.tsx
@@ -1,18 +1,23 @@
 import { Menubar } from 'primereact/menubar';
 import { MenuItem } from 'primereact/menuitem';
 
-import { memo } from 'react';
-
-
-// const Greeting = memo(function Greeting({ name }) {
-//   return <h1>Hello, {name}!</h1>;
-// });
+import { Button } from 'primereact/button';
+import { memo, Fragment } from 'react';
 
 const Navbar = memo(function Navbar() {
 
-  const start=<a href="/">
-                  <img alt="logo" src="https://routenote.com/blog/wp-content/uploads/2016/01/d8465f8c5906f540df63f7ff83e8aae6.640x360x30.gif" height="40" className="mr-2 rounded-full"></img>
-              </a>;
+  const start = (
+    <Fragment>
+      <div className="flex flex-row gap-2">
+          <a href="/">
+            <img alt="logo" src="https://routenote.com/blog/wp-content/uploads/2016/01/d8465f8c5906f540df63f7ff83e8aae6.640x360x30.gif" height="40" className="mr-2 rounded-full"></img>
+          </a>
+          {/* <span className="text-3xl"> Maestri</span> */}
+      </div>
+    </Fragment>
+  );
+  
+  const end=<Button severity='secondary'>About</Button>;
 
   const items: MenuItem[] = [
     {
@@ -41,7 +46,7 @@ const Navbar = memo(function Navbar() {
   // Return
   return (
     <div>
-      <Menubar model={items} start={start}/>
+      <Menubar model={items} start={start} end={end}/>
     </div>
   )
 }); 
