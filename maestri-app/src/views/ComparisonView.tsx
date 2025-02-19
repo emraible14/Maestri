cb72dd8995e40742059bbbae098f18a6862d5e01
Emma <emraible@uw.edu>
2025-02-19 11:40:51 +0100

    initial work

--- maestri-app/src/views/ComparisonView.tsx
+++ maestri-app/src/views/ComparisonView.tsx
@@ -1,9 +1,56 @@
-function Comparison() {
+import { useState } from "react";
+import { Artist } from "../utils/interfaces";
+
+function Comparison(props) {
+
+    const [artists, setArtists] = useState(props.artists || []);
+
+    if (artists.length === 0) {
+      setArtists([
+        {
+          id: 1,
+          name: "Eminem",
+          image: "https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1114445501.jpg",
+        },
+        {
+          id: 2,
+          name: "Taylor Swift",
+          image: "https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1114445501.jpg",
+        },
+        {
+          id: 3,
+          name: "Madonna",
+          image: "https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1114445501.jpg",
+        },
+        {
+          id: 4,
+          name: "Avicii",
+          image: "https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1114445501.jpg",
+        },
+        {
+          id: 5,
+          name: "Beyonce",
+          image: "https://www.shutterstock.com/image-vector/blank-avatar-photo-place-holder-600nw-1114445501.jpg",
+        },
+      ]);
+    }
+
     return (
-      <div>
-        <h1>Comparison Page </h1>
+      <>
+        <h1>Compare Artists</h1>
+
+        <div className="flex flex-row">
+          { artists.map(singleArtist) }
+        </div>
+      </>
+    );
+
+    function singleArtist(artist: Artist) {
+
+      return <div key={artist.id}>
+        <h2>{ artist.name }</h2>
       </div>
-    )
+    }
   }
   
   export default Comparison
