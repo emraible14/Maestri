db3249b954383ab63e86405325f480c5874ea107
meya <meya@kth.se>
2025-02-26 14:14:25 +0100

    Updated homepage

--- maestri-app/src/views/Home.tsx
+++ maestri-app/src/views/Home.tsx
@@ -3,8 +3,11 @@ import { Card } from "primereact/card";
 
 function Home() {
 
-  const header = (
+  const mapHeader = (
     <img alt="Card" className="" src="https://images.genius.com/073372f6cd316f7c68b4c4b7d8c610c9.675x675x1.jpg"/>
+  );
+  const artistHeader = (
+    <img alt="Card" src="https://images.genius.com/df3ebb0ffe60340ec5ca9b5139c24649.675x675x1.jpg"/>
   );
 
 
@@ -12,20 +15,21 @@ function Home() {
     <div>
       <div className="flex flex-col justify-center items-center">
         <h1>Maestri App Home</h1>
+        <p>Welcome to Maestri your one-stop shop for comparing artists, here you'll find who's contributing to who.</p>
+        <h4>Get started!</h4>
       </div>
-      <p>Welcome to Maestri your one-stop shop for comparing artists</p>
       
-      <div className="flex flex-row justify-center w-90% h-60% gap-10" >
-        <a href="/mapview" className="w-1/4 no-underline">
-          <Card title="Map" subTitle="See how your favorite artist charts around the world" header={
-              <img alt="Card" src="https://images.genius.com/073372f6cd316f7c68b4c4b7d8c610c9.675x675x1.jpg"/>
-            }/>
-        </a>
-        <a href="/artist" className="w-1/4 no-underline">
-          <Card title="Artist" subTitle="Checkout a random artist and their contributors" header={
-              <img alt="Card" src="https://images.genius.com/df3ebb0ffe60340ec5ca9b5139c24649.675x675x1.jpg"/>
-            }/>
-        </a>
+      <div className="flex flex-row justify-center" >
+        <div className="flex flex-row w-1/2">
+          <a href="/mapview" className="no-underline">
+            <Card title="Map" subTitle="See how your favorite artist charts around the world" header={mapHeader}/>
+          </a>
+          <div className="w-1/10"/>
+          <a href="/artist" className="no-underline">
+            <Card title="Artist" subTitle="Checkout a random artist and their contributors" header={artistHeader}/>
+          </a>
+        </div>
+
       </div>
     </div>
   )
