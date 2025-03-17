8639e8728512ba7748442cc182813c60ba9b41b1
Andrei Daniel Balanica <danielbalanica.dev@gmail.com>
2025-03-17 16:38:31 +0100

    draft of about page

--- maestri-app/src/components/Navbar.tsx
+++ maestri-app/src/components/Navbar.tsx
@@ -6,12 +6,17 @@ function Navbar() {
   const navigate = useNavigate();
 
   const start = (
-      <a href="/">
+      <a onClick={() =>  navigate('/')}>
         <img alt="logo" src="https://routenote.com/blog/wp-content/uploads/2016/01/d8465f8c5906f540df63f7ff83e8aae6.640x360x30.gif" height="40" className="mr-2 rounded-full"></img>
       </a>  
   );
 
   const items: MenuItem[] = [
+    {
+      label: 'About',
+      icon: 'pi pi-info-circle',
+      command: () =>  navigate('/about')
+    },
     {
       label: 'Artist',
       icon: 'pi pi-user',
