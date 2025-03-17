9543f78ac24906d128f13d5faa51a87de0bec458
Isak Larsson <isak.valter.larsson@gmail.com>
2025-03-18 00:01:15 +0100

    Add node tooltip to network graph

--- maestri-app/src/components/NetworkChart.tsx
+++ maestri-app/src/components/NetworkChart.tsx
@@ -1,9 +1,11 @@
-import {InputNode, ResponsiveNetwork } from "@nivo/network"
+import { InputNode, NodeTooltipProps, ResponsiveNetwork } from "@nivo/network"
+import { ResponsivePie } from '@nivo/pie'
 import NetWorkNodeComponent from "./NetworkNodeComponent";
 import {Artist, NetworkNode} from "../utils/interfaces";
 import { DataModel } from "../DataModel";
-import {getColorPalette} from "../utils/colorUtilities.ts";
-import {useEffect, useMemo, useRef} from "react";
+import { getColorPalette, nivoDarkColorPalette } from "../utils/colorUtilities.ts";
+import { useEffect, useMemo, useRef } from "react";
+import { Card } from "primereact/card";
 
 interface NetworkChartProps {
     readonly model: DataModel;
@@ -40,6 +42,126 @@ function NetworkChart(props: NetworkChartProps) {
         }
     }, [artistNetwork]);
 
+    const artistNodeTooltip = (node: NodeTooltipProps<NetworkNode>) => {
+        const artist = props.model.getArtist(node.node.data.id)
+        // @ts-ignore
+        const CenteredMetric = ({ dataWithArc, centerX, centerY }) => {
+            let total = 0
+
+            // @ts-ignore
+            dataWithArc.forEach(datum => {
+                total += datum.value
+            })
+
+            return (
+                <text
+                    x={centerX}
+                    y={centerY}
+                    textAnchor="middle"
+                    dominantBaseline="central"
+                    fill='white'
+                    style={{
+                        fontSize: '24px',
+                    }}
+                >
+                    {total}
+                </text>
+            )
+        }
+
+
+        const contributionTypesCounts = artist.contributions.reduce((acc, e) => (acc.set(e.type, 1 + (acc.get(e.type) || 0))), new Map<string, number>)
+
+        var data = [
+            {
+                "id": "PD",
+                "label": "producer",
+                "value": (contributionTypesCounts.get("producer") || 0),
+                "color": nivoDarkColorPalette["#1b9e77"][0]
+            },
+            {
+                "id": "PR",
+                "label": "primary",
+                "value": (contributionTypesCounts.get("primary") || 0),
+                "color": nivoDarkColorPalette["#d95f02"][0]
+            },
+            {
+                "id": "FT",
+                "label": "feature",
+                "value": (contributionTypesCounts.get("feature") || 0),
+                "color": nivoDarkColorPalette["#7570b3"][0]
+            },
+            {
+                "id": "WR",
+                "label": "writer",
+                "value": (contributionTypesCounts.get("writer") || 0),
+                "color": nivoDarkColorPalette["#e7298a"][0]
+            },
+        ].filter((d) => d["value"] > 0)
+
+        // @ts-ignore
+        const PieChart = ({ data /* see data tab */ }) => (
+            <ResponsivePie
+                data={data}
+                margin={{ top: 10, right: 35, bottom: 10, left: 35 }}
+                innerRadius={0.5}
+                padAngle={0.7}
+                cornerRadius={3}
+                activeOuterRadiusOffset={8}
+                borderWidth={1}
+                borderColor={{
+                    from: 'color',
+                    modifiers: [
+                        [
+                            'darker',
+                            0.2
+                        ]
+                    ]
+                }}
+                arcLinkLabelsSkipAngle={10}
+                arcLinkLabelsTextColor="white"
+                arcLinkLabelsThickness={2}
+                arcLinkLabelsColor={{ from: 'color' }}
+                arcLabelsSkipAngle={10}
+                arcLinkLabelsDiagonalLength={5}
+                arcLinkLabelsStraightLength={5}
+                arcLabelsTextColor={'white'}
+                layers={['arcs', 'arcLabels', 'arcLinkLabels', 'legends', CenteredMetric]}
+                colors={{
+                    datum: 'data.color'
+                }}
+            />
+        )
+
+        return (
+            <Card style={{
+                'borderRadius': "0%",
+            }}>
+                <div className='flex flex-row' style={{ gap: '1rem' }}>
+                    <div className="flex flex-col" style={{ gap: '0.3rem' }}>
+                        <div style={{ height: "9rem", width: "9rem" }}>
+                            <img src={artist.image_url} style={{ height: "100%", width: "100%", objectFit: "cover", borderRadius: "5%" }} alt={artist.name}></img>
+                        </div>
+                        {<><a style={{ fontSize: "80%" }}>PD - producer credit</a>
+                            <a style={{ fontSize: "80%" }}>PR - primary artist</a>
+                            <a style={{ fontSize: "80%" }}>FT - featured artist</a>
+                            <a style={{ fontSize: "80%" }}>WR - writer credit</a>
+                        </>}
+                    </div>
+                    <div className="flex flex-col" style={{ gap: '0.0rem', flexWrap: "wrap" }}>
+                        <a className="artist-name-link" style={{ fontSize: "120%" }}>{artist.name}</a>
+
+                        <a className="text-wrap" style={{ fontSize: "90%" }}> has {artist.contributions.length} credits across all tracks</a>
+                        <div style={{ height: "20vh", width: "20vh" }}>{PieChart({ data: data })}
+                        </div>
+
+                    </div>
+                </div>
+            </Card>
+        )
+    }
+
+
 
     return (
         <div ref={scrollRef} id={"networkGraph"} style={{height: "72vh", width: "100%", overflowX: "scroll", overflowY:"scroll"}}>
@@ -69,19 +191,8 @@ function NetworkChart(props: NetworkChartProps) {
                 linkColor={"#374151"}
                 motionConfig="slow"
                 onClick={n => props.clickedNode(n.id)}
-                nodeComponent={n=> NetWorkNodeComponent(n, props.model)}
-                    nodeTooltip={(node)=>{
-                    const id = node.node.data.id
-                    let name = id
-                    if (typeof props.artist == 'undefined'){
-                        // name = "Name not found!"
-                    }
-                    else {
-                        name = props.model.getArtist(node.node.data.id).name
-                    }
-
-                    return <div style={{backgroundColor: "#374151", borderRadius: "5px", padding: "5px"}}>{name}</div>
-                }}
+                nodeComponent={n => NetWorkNodeComponent(n, props.model)}
+                nodeTooltip={artistNodeTooltip}
                 distanceMin={20}
             />
         </div>
