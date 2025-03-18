744ef8766e6304f06367ba2228123e5610ae75f7
meya <meya@kth.se>
2025-03-18 20:48:21 +0100

    Updated to hashrouter for the github deployment

--- maestri-app/src/App.tsx
+++ maestri-app/src/App.tsx
@@ -2,9 +2,9 @@
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
-import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { HashRouter, Route, Routes} from "react-router-dom";
 
-import Home from './views/Home';
+import Home from './views/HomeView';
 import Artist from './views/ArtistView';
 import Comparison from './views/ComparisonView';
 import Network from './views/NetworkView';
@@ -14,7 +14,7 @@ import { DataModel } from './DataModel';
 function App(props: { readonly model: DataModel }) {
     return (
         <div>
-        <BrowserRouter>
+        <HashRouter>
             <Navbar/>
             <Routes>
                 <Route path="/" element={<Home/>}></Route>
@@ -22,7 +22,7 @@ function App(props: { readonly model: DataModel }) {
                 <Route path="/comparison" element={<Comparison model={props.model}/>}></Route>
                 <Route path="/network" element={<Network model={props.model}/>}></Route>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
         </div>
     )
 }
